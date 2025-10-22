3379410af6eb32b9fad1eca0ca94810bd821abc6
Loic2888 <12188@holbertonstudents.com>
2025-10-22 14:06:31 +0200

    fix: change comments for variableé

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,16 +2,16 @@
 
 /**
  * print_alphabet - prints the alphabet in lowercase
- *
+ *@void: character to print
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        _putchar(c);
-    }
-    _putchar('\n');
+		for (c = 'a'; c <= 'z'; c++)
+		{
+		_putchar(c);
+		}
+	_putchar('\n');
 }
