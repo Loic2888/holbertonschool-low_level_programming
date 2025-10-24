2bc05dc578dc64868ec5b02cc308c099c8c1a5f6
Loic2888 <12188@holbertonstudents.com>
2025-10-24 11:58:48 +0200

    fix: betty: white space error

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,21 +15,21 @@ void times_table(void)
 		{
 			c = a * b;
 			if (b != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 
-            if (c < 10)
-            {
-                if (b != 0)
-                    _putchar(' ');  
-                _putchar(c + '0');
-            }
-            else
-            {
-                _putchar((c / 10) + '0');  
-                _putchar((c % 10) + '0');
+			if (c < 10)
+			{
+				if (b != 0)
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 		}
 		_putchar('\n');
