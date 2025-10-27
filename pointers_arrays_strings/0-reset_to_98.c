4016902169fc28293c654d0d64cb90267a84c8ca
Loic2888 <12188@holbertonstudents.com>
2025-10-27 15:53:45 +0100

    feat: function that updates the value of a pointer to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of n to 98
+ * @n: Pointer to the integer to be updated
+ *
+ * Return: None
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
