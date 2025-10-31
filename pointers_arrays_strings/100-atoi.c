ab62ded570cd4031a4a40955540c0bc32030c745
Loic2888 <12188@holbertonstudents.com>
2025-10-31 14:54:30 +0100

    fix: add a description

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ *
+ * Return: the integer value of the converted string
+ */
+
 int _atoi(char *s)
 {
 	int sign = 1;
