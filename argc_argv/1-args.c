7d3396e3fdd3f8312818940ad3f39e25c2ee410c
Loic2888 <12188@holbertonstudents.com>
2025-11-06 14:43:06 +0100

    feat: function that prints the number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: Argument count (not used here)
+ * @argv: Argument vector (array of strings)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("Number of arguments: %d\n", argc - 1);
+	return (0);
+}
