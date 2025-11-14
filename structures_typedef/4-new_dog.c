7fca29d8448f2fbf255181a386f3cea12edc5403
Loic2888 <12188@holbertonstudents.com>
2025-11-14 11:13:11 +0100

    fix: i can't use string lib, so i add function :'(

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,38 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	return (len);
+}
+/**
+ * _strcpy - copies a string from src to dest
+ * @dest: destination pointer
+ * @src: source pointer
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
@@ -19,21 +50,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = malloc(strlen(name) + 1);
+	dog->name = malloc(_strlen(name) + 1);
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	strcpy(dog->name, name);
+	_strcpy(dog->name, name);
 	dog->age = age;
-	dog->owner = malloc(strlen(owner) + 1);
+	dog->owner = malloc(_strlen(owner) + 1);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	strcpy(dog->owner, owner);
+	_strcpy(dog->owner, owner);
 	return (dog);
 }
