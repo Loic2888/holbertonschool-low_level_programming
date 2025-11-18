050a7c4b5786ac06d02018b561d5a99790fd284b
Loic2888 <12188@holbertonstudents.com>
2025-11-18 13:37:25 +0100

    fix: use command strcmp on i

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -14,15 +14,13 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}};
+	int i;
 
-	int i = 0;
-
-	while (ops[i].op != NULL && ops[i].op[0] != s[0])
+	while (ops[i].op != NULL)
 	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
 		i++;
-
-		if (ops[i].op == NULL)
-			return (NULL);
 	}
-	return (ops[i].f);
+	return (NULL);
 }
