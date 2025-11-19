27edee94a73d10161ca110fe861629ec48e39a8f
Loic2888 <12188@holbertonstudents.com>
2025-11-19 17:50:34 +0100

    fix: fail betty, to many line in the function. try with two function

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,44 +1,73 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 /**
- * print_all - Prints arguments of various types
- * @format: A list of types of arguments passed to the function
+ * print_arg - prints a single argument based on its type
+ * @type: type of the argument ('c', 'i', 'f', 's')
+ * @args: list of variadic arguments
+ * @sep: separator to print before the argument
+ *
+ * Description: This function handles printing of one argument depending
+ * on its type. It supports characters, integers, floats and strings.
+ * For strings, if the value is NULL, it prints "(nil)".
+ */
+
+void print_arg(char type, va_list args, char *sep)
+{
+	char *str;
+
+	switch (type)
+	{
+	case 'c':
+		printf("%s%c", sep, va_arg(args, int));
+		break;
+
+	case 'i':
+		printf("%s%d", sep, va_arg(args, int));
+		break;
+
+	case 'f':
+		printf("%s%f", sep, va_arg(args, double));
+		break;
+
+	case 's':
+		str = va_arg(args, char *);
+		switch (!str)
+		{
+		case 1:
+			printf("%s(nil)", sep);
+			break;
+		default:
+			printf("%s%s", sep, str);
+			break;
+		}
+		break;
+
+	default:
+		break;
+	}
+}
+
+/**
+ * print_all - prints anything depending on format
+ * @format: list of types of arguments passed
  */
 void print_all(const char *const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char *str;
 	char *sep = "";
 
 	va_start(args, format);
+
 	while (format && format[i])
 	{
 		switch (format[i])
 		{
 		case 'c':
-			printf("%s%c", sep, va_arg(args, int));
-			sep = ", ";
-			break;
 		case 'i':
-			printf("%s%d", sep, va_arg(args, int));
-			sep = ", ";
-			break;
 		case 'f':
-			printf("%s%f", sep, va_arg(args, double));
-			sep = ", ";
-			break;
 		case 's':
-			str = va_arg(args, char *);
-			switch (!str) /* 0 = vrai string, 1 = NULL */
-			{
-			case 1:
-				printf("%s(nil)", sep);
-				break;
-			default:
-				printf("%s%s", sep, str);
-				break;
-			}
+			print_arg(format[i], args, sep);
 			sep = ", ";
 			break;
 		default:
@@ -46,6 +75,7 @@ void print_all(const char *const format, ...)
 		}
 		i++;
 	}
+
 	printf("\n");
 	va_end(args);
 }
