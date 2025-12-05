30eab82d8e5fe9a17d3750406031b3702b5df46c
Loic2888 <12188@holbertonstudents.com>
2025-12-05 14:12:47 +0100

    fix: create is not allowed, use open

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL || *filename == '\0')
 		return (-1);
-	fd = creat(filename, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
